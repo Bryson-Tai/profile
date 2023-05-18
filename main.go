dcf53573a8cf9372e4473bb4e99e48cd538459b6
Bryson Tai <brysontai10@gmail.com>
2023-05-18 22:30:27 +0800

    Add comment

--- main.go
+++ main.go
@@ -21,23 +21,25 @@ func main(){
 
 	//? Specify the routing path
 	//? The func(ctx *gin.Context) is where if user route "/", what should Gin does
-
-	//? ctx.HTML would response the status is 200 (http.StatusOK)
-	//? Route the request to index.html
-	//? Then gin.H is to provide the "key":"value", 
-	//? "key" is use for match the gomplate tag, "value" is the value of the key
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.html", gin.H{
-			"content": "This is an index page...",
-		})
-	})
-
-	router.GET("/about", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "about.html", gin.H{
-			"content": "This is an about page...",
-		})
-	})
+	router.GET("/", indexHandler)
+	router.GET("/about", aboutHandler)
 
 	//? Start to run the server with address and port
 	router.Run("localhost:8080")
+}
+
+//? 1. ctx.HTML would response the status is 200 (http.StatusOK)
+//? 2. Route the request to index.html
+//? 3.1. Then gin.H is to provide the "key":"value", 
+//? 3.2. "key" is use for match the gomplate tag, "value" is the value of the key
+func indexHandler(ctx *gin.Context){
+	ctx.HTML(http.StatusOK, "index.html", gin.H{
+		"content": "This is an index page...",
+	})
+}
+
+func aboutHandler(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "about.html", gin.H{
+		"content": "This is an about page...",
+	})
 }
