eaef840f58343ddd18ae39a635da16af7691e62e
Bryson Tai <brysontai10@gmail.com>
2023-05-18 22:22:20 +0800

    Simple Gin Use Case

---
+++ exec.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+gin -i --appPort 8080 --all run main.go
--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import(
-
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+	"strings"
 )
 
 func main(){
+	router := gin.Default()
+
+	router.SetFuncMap(template.FuncMap{
+		"upper": strings.ToUpper,
+	})
+
+	router.LoadHTMLGlob("web/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"content": "This is an index page...",
+		})
+	})
+
+	router.GET("/about", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "about.html", gin.H{
+			"content": "This is an about page...",
+		})
+	})
+
+	router.Run("localhost:8083")
 	
 }
