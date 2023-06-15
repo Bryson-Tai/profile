99983b431e8bb2f97ac33ac67fdd17978eecfe32
Bryson Tai <brysontai10@gmail.com>
2023-06-16 00:54:53 +0800

    Debug K3S installation and user_data issue

--- terraform/modules/install_k3s.sh
+++ terraform/modules/install_k3s.sh
@@ -3,6 +3,13 @@ hostIP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 sudo apt update -y 
 
-mkdir ~/.kube && curl -sfL https://get.k3s.io | "INSTALL_K3S_EXEC=\"--write-kubeconfig=/home/ubuntu/.kube/k3s_kubeconfig.yaml --write-kubeconfig-mode=644 --node-external-ip=$hostIP\" sh -"
+mkdir ~/.kube
 
-echo "K3s successfully installed..."
+if [ $? -eq 0 ]; then
+
+    curl -sfL https://get.k3s.io | "INSTALL_K3S_EXEC=\"--write-kubeconfig=/home/ubuntu/.kube/k3s_kubeconfig.yaml --write-kubeconfig-mode=644 --node-external-ip=$hostIP\" sh -"
+
+    echo "K3s successfully installed..."
+    
+fi
+
